cf53db289c09e9f8021da801385eebdc51d8cbc6
unknown <rhrabin934602@gmail.com>
2022-07-05 19:51:39 +0600

    servicer section added

--- src/Components/Service.js
+++ src/Components/Service.js
@@ -2,40 +2,43 @@ import React from 'react';
 import { BiCodeAlt } from 'react-icons/bi';
 import { FaReact } from 'react-icons/fa';
 import { IoServer } from 'react-icons/io5';
-
+import './service.css'
 const Service = () => {
 
 
     return (
-        <div>
-            <h1 className='text-center my-10'>
-                <span className='text-4xl text-center px-4  text-secondary border-x-4 border-pink-300 '>My  Services</span>
+        <div style={{ backgroundColor: '#111', color: '#fff' }} className=' h-[80vh] '>
+            <h1 className='text-center pt-10 mb-16'>
+                <h1 className='text-4xl text-center text-white title relative mb-30 pb-8 font-mono'>My  Services</h1>
+
             </h1>
             <div className='px-2 md:px-20 lg:px-60 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'
             >
 
-                <section class="card lg:w-96 bg-base-100 shadow-2xl hover:bg-cyan-100 hover:text-black"
+                <section style={{ backgroundColor: '#222' }} class="card card-main   shadow-2xl "
                     data-aos="fade-left"
                     data-aos-duration="1500"
 
                 >
-                    <figure class="px-10 pt-10"
-                    >
-                        <FaReact style={{ fontSize: '2.5em', color: 'cyan' }} />
-                    </figure>
-                    <div class="card-body items-center text-center">
-                        <h2 class="text-2xl text-primary">React Developer</h2>
-                        <p>I am a React Developer. I have also created many single page application using React js. I can be your React Developer.</p>
+                    <div className='card-transition'>
+                        <figure class="px-10 pt-10"
+                        >
+                            <FaReact style={{ fontSize: '2.5em', color: 'crimson' }} />
+                        </figure>
+                        <div class="card-body items-center text-center">
+                            <h2 class="text-2xl font-bold">React Developer</h2>
+                            <p>I am a React Developer. I have also created many single page application using React js. I can be your React Developer.</p>
 
+                        </div>
                     </div>
                 </section>
-                <section class="card lg:w-96 bg-base-100 shadow-2xl hover:bg-cyan-100 hover:text-black"
+                <section style={{ backgroundColor: '#222' }} class="card  shadow-2xl "
                     data-aos="fade-up"
                     data-aos-duration="1500">
                     <figure class="px-10 pt-10"
 
                     >
-                        <BiCodeAlt style={{ fontSize: '2.5em', color: '#FDE68A' }} />
+                        <BiCodeAlt style={{ fontSize: '2.5em', color: 'crimson' }} />
                     </figure>
                     <div class="card-body items-center text-center">
                         <h2 class="text-2xl text-primary">Full Stack Web Developer</h2>
@@ -43,12 +46,12 @@ const Service = () => {
                         .
                     </div>
                 </section>
-                <section class="card lg:w-96 bg-base-100 shadow-2xl hover:bg-cyan-100 hover:text-black"
+                <section style={{ backgroundColor: '#222' }} class="card   shadow-2xl"
                     data-aos="fade-right"
                     data-aos-duration="1000"
                 >
                     <figure class="px-10 pt-10 ">
-                        <IoServer style={{ fontSize: '2.5em', color: 'green' }} />
+                        <IoServer style={{ fontSize: '2.5em', color: 'crimson' }} />
                     </figure>
                     <div class="card-body items-center text-center">
                         <h2 class="text-2xl text-primary">Backend Developer</h2>
